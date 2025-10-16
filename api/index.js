8e2ffa52c38a58582f10c4210b3e9e27905b34e5
Ange1809 <10kaisukebaji@gmail.com>
2025-10-16 01:12:42 -0300

    404

--- api/index.js
+++
@@ -1,6 +0,0 @@
-// api/index.js
-const { app } = require('../tateti');
-
-// Exporta la aplicación Express directamente. 
-// Vercel usará esto para manejar todas las rutas definidas en 'app'.
-module.exports = app;
